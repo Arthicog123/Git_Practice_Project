f5041cc6e745d17757bf525d78f90f146a147a0b
Arthicog123 <arthilakshmi369@gmail.com>
2026-02-20 12:42:28 +0530

    second commit

---
+++ src/test/java/practicepackage/AmazonTest.java
@@ -0,0 +1,29 @@
+package practicepackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AmazonTest {
+
+	@Test
+	public void name() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		
+		driver.get("https://www.amazon.in/");
+		
+		driver.findElement(By.xpath("//div[@id='icp-nav-flyout']/button[@class='nav-flyout-button nav-icon nav-arrow']")).click();
+		 String lang = "MR";
+	
+		driver.findElement(By.xpath("//div[@id='nav-flyout-icp']/descendant::span[text()='"+lang+"']/preceding-sibling::i[@class='icp-radio']")).click();
+		
+		driver.quit();
+	}
+	
+}
